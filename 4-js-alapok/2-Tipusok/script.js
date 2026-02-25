39b51f12cde89882f57e9847d40c5e5143923035
farkas.norbert <farkas.norbert@ckik.hu>
2026-02-25 15:51:50 +0100

    JS - console.log, típusok

---
+++ 4-js-alapok/2-Tipusok/script.js
@@ -0,0 +1,23 @@
+let a = 5;
+console.log("alma");
+console.clear();
+
+// Típusok
+
+// Tipp: string, char, int, float, double, bool, List<>, struct
+
+// Valóság:
+// string, char -> string
+// int, float, double -> number
+// bool -> boolean
+    // array, List<> -> array (python)
+    // struct -> object
+
+// 1. Szöveg
+console.log("1. Szöveg:", "5", typeof("5"));
+
+// 2. Szám
+console.log("2. Szám:", 5, 3.14, typeof(5), typeof(3.14));
+
+// 3. Logikai
+console.log("3. Logikai:", true, typeof(true));
