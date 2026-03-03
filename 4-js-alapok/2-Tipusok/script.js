c163d77d7cb07e2c17f7879513c23937d7763c03
farkas.norbert <farkas.norbert@ckik.hu>
2026-03-03 20:18:59 +0100

    JS alapok - Típusok, műveletek

--- 4-js-alapok/2-Tipusok/script.js
+++ 4-js-alapok/2-Tipusok/script.js
@@ -21,3 +21,38 @@ console.log("2. Szám:", 5, 3.14, typeof(5), typeof(3.14));
 
 // 3. Logikai
 console.log("3. Logikai:", true, typeof(true));
+
+// ---------------------------------------------------
+let b = 5;
+let c;
+
+// 4. Undefined
+// Kezdőérték nélküli változók.
+// Return nélküli függvények visszatérési értéke.
+console.log("4. Undefined:", c, typeof(c));
+
+// 5. NaN (Not A Number)
+// Undefined értékkel végzett művelet.
+// Number("alma")
+console.log("5. NaN:", b + c, typeof(NaN));
+
+// -----------------------------------------
+// Konverziók
+
+console.log(Number("5")); // 5
+console.log(String(5)); // "5"
+
+// truthy
+console.log(Boolean(5)); // true
+console.log(Boolean("alma")); // true
+
+// falsy
+console.log(Boolean(0)); // false
+console.log(Boolean("")); // false
+console.log(Boolean(undefined)); // false
+console.log(Boolean(NaN)); // false
+console.log(Boolean(false)); // false
+console.log(Boolean(null)); // false
+
+// Lehetetlen konverzió
+console.log(Number("alma")); // NaN
---
+++ 4-js-alapok/3-Muveletek/script.js
@@ -0,0 +1,9 @@
+// Operátorok - műveletek
+
+// 1. Aritmetikai
+console.log("Összeg:", 2 + 3);
+console.log("Kivonás:", 5 - 3);
+console.log("Szorzás:", 2 * 3);
+console.log("Osztás:", parseFloat((17 / 3).toFixed(2))); // kerekítés
+console.log("Hányados:", parseInt(17 / 3)); // egész osztás
+console.log("Maradék:", 17 % 3);
