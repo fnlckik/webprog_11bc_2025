b74f6166e8add3f33773cd90a7411347ded76ce0
farkas.norbert <farkas.norbert@ckik.hu>
2026-04-21 19:52:34 +0200

    WPF

--- 5-DOM/4-gyumolcsok/elemek.js
+++ 5-DOM/4-gyumolcsok/elemek.js
@@ -10,13 +10,26 @@ const TESCO = "https://bevasarlas.tesco.hu/groceries/hu-HU/shop/zoldseg-gyumolcs
 // Elemek
 
 // H1 elem - Gyümölcsök
+const h1 = document.createElement("h1"); // Létrehozás
+// h1.textContent = "Gyümöl\ncsök";
+h1.innerText = "Gyümölcsök"; // Módosítás
 
-
-
+// const body = document.body;
+const body = document.querySelector("body");
+body.appendChild(h1); // Beillesztem a DOM fába
+// const h1masik = document.createElement("h1");
+// h1masik.innerText = "Gyümölcsök";
+// body.append(h1, h1masik);
 
 // Kommentek eltüntetése
+const head = document.querySelector("head");
+// const comment = head.childNodes[head.childNodes.length-2];
+const comment = document.head.lastChild.previousSibling;
+head.removeChild(comment);
+// comment.remove();
 
-
+const comment2 = document.body.childNodes[1];
+body.removeChild(comment2);
 
 
 // Hivatkozás - TESCO
