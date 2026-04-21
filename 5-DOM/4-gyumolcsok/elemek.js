f53fe867db97ea1c388b4e67d49b15df1d3059e4
farkas.norbert <farkas.norbert@ckik.hu>
2026-04-21 12:49:12 +0200

    DOM gyumolcsok forrasok

---
+++ 5-DOM/4-gyumolcsok/elemek.js
@@ -0,0 +1,45 @@
+// Konstansok
+const szoveg = "A nyelv gyümölcsnek nevezi a növények édes és húsos termését.";
+const gyumolcsok = ["Alma", "Körte", "Eper", "Narancs", "Szőlő"];
+const kedvencek = [true, false, true, false, false];
+const TESCO = "https://bevasarlas.tesco.hu/groceries/hu-HU/shop/zoldseg-gyumolcs/gyumolcsok/all";
+
+
+
+// -----------------------------------------------------------
+// Elemek
+
+// H1 elem - Gyümölcsök
+
+
+
+
+// Kommentek eltüntetése
+
+
+
+
+// Hivatkozás - TESCO
+
+
+
+
+// Bekezdés
+
+
+
+
+// Kép - gyumolcsok.jpg
+
+
+
+
+// Lista
+
+
+
+
+// Bekezdés - strong (alapműveletekkel)
+
+
+// Másképp:
---
+++ 5-DOM/4-gyumolcsok/stilusok.js
@@ -0,0 +1,24 @@
+// -----------------------------------------------------------
+// Stílusok
+
+// Naming conventions
+// Python, SQL (mezők): snake_case => jegyek_szama
+// C#: PascalCase => Console.WriteLine()
+// CSS: kebab-case, dash-case, css-case... => text-align
+// JS: camelCase => textAlign
+
+
+
+
+
+// ----------------------------------------------------
+// .kedvenc osztályú elemek formázása
+
+// MO1: HTMLCollection-ként kiválasztva
+
+
+// MO2: NodeList-ként kiválasztva
+
+
+// MO3: ???
+
