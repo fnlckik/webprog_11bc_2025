b66a62642d745118ae96ab921a752b65eca1ea80
farkas.norbert <farkas.norbert@ckik.hu>
2026-04-27 10:17:56 +0200

    DOM - createElement, appendChild

--- 5-DOM/4-gyumolcsok/elemek.js
+++ 5-DOM/4-gyumolcsok/elemek.js
@@ -4,8 +4,6 @@ const gyumolcsok = ["Alma", "Körte", "Eper", "Narancs", "Szőlő"];
 const kedvencek = [true, false, true, false, false];
 const TESCO = "https://bevasarlas.tesco.hu/groceries/hu-HU/shop/zoldseg-gyumolcs/gyumolcsok/all";
 
-
-
 // -----------------------------------------------------------
 // Elemek
 
@@ -33,26 +31,70 @@ body.removeChild(comment2);
 
 
 // Hivatkozás - TESCO
+// HTML attribútum <=> JS adattag
 
-
-
+const a = document.createElement("a");
+a.innerText = "TESCO";
+a.href = TESCO;
+a.target = "_blank";
+body.appendChild(a);
 
 // Bekezdés
 
-
-
+const p = document.createElement("p");
+// p.innerText = szoveg;
+// body.appendChild(p); // Nem jó helyre kerül
+// Beszúrja a <p> elemet a <body>-ba az <a> elé
+// a.insertAdjacentElement("beforebegin", p);
+// a.before(p);
+body.insertBefore(p, a);
 
 // Kép - gyumolcsok.jpg
+// img.setAttribute("width", 280);
+// img.setAttribute("alt", "gyumolcsok");
 
-
-
+const img = document.createElement("img");
+img.src = "kepek/gyumolcsok.jpg";
+img.width = 280;
+img.alt = "gyumolcsok";
+img.title = "Vegyes gyümölcsök";
+p.after(img);
 
 // Lista
+// li.className = "felkover piros";
+// li.classList.add("felkover");
+// li.classList.add("piros");
 
-
-
+const ul = document.createElement("ul");
+for (let i = 0; i < gyumolcsok.length; i++) {
+    const li = document.createElement("li");
+    li.innerText = gyumolcsok[i];
+    if (kedvencek[i]) {
+        // Baj: a class kulcsszó foglalt a nyelvben!
+        // li.class = "kedvenc";
+        // li.setAttribute("class", "kedvenc");
+        // li.className = "kedvenc";
+        li.classList.add("kedvenc");
+    }
+    ul.appendChild(li);
+}
+img.after(ul);
 
 // Bekezdés - strong (alapműveletekkel)
 
+// p.innerText = "A nyelv <strong>gyümölcs</strong>nek";
+const t1 = document.createTextNode("A nyelv ");
+const strong = document.createElement("strong");
+const t2 = document.createTextNode("nek nevezi a növények édes és húsos termését.");
+strong.innerText = "gyümölcs";
+// p.appendChild(t1);
+// p.appendChild(strong);
+// p.appendChild(t2);
+p.append(t1, strong, t2);
+
+// p.innerText = "";
 
 // Másképp:
+// p.append(szoveg.split("gyümölcs")[0], strong, szoveg.split("gyümölcs")[1]);
+
+p.innerHTML = "A nyelv <strong>gyümölcs</strong>nek nevezi a növények édes és húsos termését.";
--- 5-DOM/4-gyumolcsok/stilusok.js
+++ 5-DOM/4-gyumolcsok/stilusok.js
@@ -7,9 +7,14 @@
 // CSS: kebab-case, dash-case, css-case... => text-align
 // JS: camelCase => textAlign
 
+h1.style.border = "5px solid green";
+h1.style.padding = "10px";
+h1.style.textAlign = "center";
+h1.style.backgroundColor = "lightgreen";
 
-
-
+img.style.borderRadius = "30px";
+img.style.float = "right";
+img.style.transform = "rotate(-5deg)";
 
 // ----------------------------------------------------
 // .kedvenc osztályú elemek formázása
