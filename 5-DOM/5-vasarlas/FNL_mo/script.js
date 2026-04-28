059450992ab744ef6a9f6b68dcc25d45bfa04d78
farkas.norbert <farkas.norbert@ckik.hu>
2026-04-28 19:33:47 +0200

    vasarlas - F1

---
+++ 5-DOM/5-vasarlas/FNL_mo/script.js
@@ -0,0 +1,82 @@
+// F1
+function beolvas(termekek) {
+    const sorok = document.querySelectorAll("ul li");
+    for (const sor of sorok) {
+        console.log(sor.innerText.split(" - "));
+    }
+}
+
+
+
+// F2
+
+
+
+
+// F3
+
+
+
+
+// F4
+
+
+
+
+// F5
+
+
+
+
+// F6
+
+
+
+
+// F7
+
+
+
+
+// F8
+
+
+
+
+// F9
+
+
+
+
+function bevasarlo(termekek) {
+    beolvas(termekek);
+    // osszesAr(termekek);
+    // kerekAr(termekek);
+    // legdragabb(termekek);
+    // dragak();
+    // diagram(termekek);
+}
+
+// Termékek listája:
+/*
+termekek = [
+    {
+        nev: "Sajt",
+        ar: 2849
+    },
+    {
+        nev: "Szalonna",
+        ar: 1637
+    },
+    ...
+]
+*/
+function main() {
+    const termekek = [];
+    const kepek = ["sajt", "kave", "tejfol"];
+    bevasarlo(termekek);
+    // kepekBeszuras(kepek);
+    // hivatkozasok();
+}
+
+main();
